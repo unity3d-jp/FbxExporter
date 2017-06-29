f54c2e890535957f264fb11fb4de7c53ec8755e0
i-saint <saint.skr@gmail.com>
2017-06-29 21:49:18 +0900

    fork from NormalPainter

---
+++ FbxExporter/Assets/FbxExporterPackaging.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+
+public class FbxExporterPackaging
+{
+    [MenuItem("Assets/Make FbxExporter.unitypackage")]
+    public static void MakePackage()
+    {
+        string[] files = new string[]
+        {
+            "Assets/UTJ/FbxExporter",
+        };
+        AssetDatabase.ExportPackage(files, "FbxExporter.unitypackage", ExportPackageOptions.Recurse);
+    }
+
+}
+#endif // UNITY_EDITOR
--- Plugin/NormalPainter/NormalPainter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-    #define npAPI extern "C" __declspec(dllexport)
-    //#define npEnablePenTablet
-#else
-    #define npAPI extern "C" 
-#endif
-
